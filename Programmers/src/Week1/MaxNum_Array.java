3c57ff76e49f2990dee0c6cc7a9d7c37068347a3
khw64 <khw64@LAPTOP-PPCB0B8V>
2022-10-27 00:50:32 +0900

    최댓값 구하기(1)

--- Programmers/src/Week1/MaxNum_Array.java
+++ Programmers/src/Week1/MaxNum_Array.java
@@ -1,5 +1,21 @@
 package Week1;
 
 public class MaxNum_Array {
-
+	
+	public int solution(int[] numbers) {
+		
+		int max = numbers[0];
+		for(int i = 1; i < numbers.length; i++) {
+			if(numbers[i] > max) max = numbers[i];
+		}
+		int m = 0;
+		int max2 = numbers[0];
+		for(int i = 1; i < numbers.length; i++) {
+			m = max2;
+			if(numbers[i] > max2) max2 = numbers[i];
+			if(max == max2) max2 = m;
+		}
+		return max * max2;
+	}
 }
+
