ac96dc5280d79a9c1a2ecda7b27619ceef1172f9
khw64 <khw64@LAPTOP-PPCB0B8V>
2022-11-01 23:08:24 +0900

    피자 나누기(2)

--- Programmers/src/week2/PizzaShare2.java
+++ Programmers/src/week2/PizzaShare2.java
@@ -2,15 +2,16 @@ package week2;
 
 public class PizzaShare2 {
 
-	public static void main(String[] args) {
-		int n = 15, i = 1;
-		int answer = 0;
+	public int solution(int n) {
+		int answer = 0, i = 1;
+		// 6조각의 피자를 n명의 사람으로 나누었을 때, 남는 피자가 없을 때까지
+		// while문 반복한다.
 		while(true) {
-            answer = 6 * i;
-            if (answer % n == 0) break;
-            i += 1;
-        }
-		System.out.println(answer / 6);
+			answer = 6 * i;
+			if (answer % n == 0) break;
+			i += 1;
+		}
+		
+		return answer / 6;
 	}
-
 }
