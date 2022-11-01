6459fda227d3ed156e4b15b89ce824d944c9de66
khw64 <khw64@LAPTOP-PPCB0B8V>
2022-11-01 23:08:33 +0900

    짝수 싫어

--- Programmers/src/week2/HateEvenNumber.java
+++ Programmers/src/week2/HateEvenNumber.java
@@ -1,5 +1,18 @@
 package week2;
 
 public class HateEvenNumber {
-
+	public int[] solution(int n) {
+		int[] answer;
+		if(n % 2 == 0){
+			answer = new int[n/2];
+		} else {
+			answer = new int[n/2 + 1];
+		}
+		int j = 1;
+		for(int i = 0; i < answer.length; i++) {
+			answer[i] = j;
+			j += 2;
+		}
+		return answer;
+	}
 }
