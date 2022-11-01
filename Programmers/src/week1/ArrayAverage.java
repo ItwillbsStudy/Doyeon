4419cb099105b38d90005c6f465695edd550c647
khw64 <khw64@LAPTOP-PPCB0B8V>
2022-11-01 22:14:57 +0900

    배열의 평균값

---
+++ Programmers/src/week1/ArrayAverage.java
@@ -0,0 +1,11 @@
+package week1;
+
+public class ArrayAverage {
+	public double solution(int[] numbers) {
+		double answer = 0;
+		for(int i = 0; i < numbers.length; i++) {
+			answer += numbers[i];
+		}
+		return answer/numbers.length;
+	}
+}
