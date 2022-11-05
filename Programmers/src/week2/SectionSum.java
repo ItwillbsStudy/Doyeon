fe945bc6d7e24367cc4d5e9d1783f847c22e5336
khw64 <khw64@LAPTOP-PPCB0B8V>
2022-11-05 14:47:48 +0900

    배열의 구간의 합

--- Programmers/src/week2/SectionSum.java
+++ Programmers/src/week2/SectionSum.java
@@ -7,25 +7,32 @@ public class SectionSum {
 	public static void main(String[] args) {
 		//a, b 입력
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+		int a = 1;//sc.nextInt();
+		int b = 5;//sc.nextInt();
 		
 		//b길이의 배열
-		int [] array = new int [b];
+		int [] array = new int [b+1];
 		
-		//미완성
 		// 1, 2, 2, 3, 3, 3, 4...수 배열에 넣기
 		int num = 0;
-		for(int i = 0; i < array.length; i++) {
-			num++;
-			for(int j = 0; j < num; j++) {
-				array[i] = num;
+		int index = 0;
+		ADD:
+		while(index < array.length) {
+			
+			if(index > b)break ADD;
+			num += 1;
+			for(int i = 0; i < num; i++) {
+				array[index] = num;
+				System.out.println(array[index]);
 			}
+			index++;
 		}
 		
-		// 입력된 a ~ b 위치 까지의 합 계tks
+//		0, 1, 2, 3,
+		
+		// 입력된 a ~ b 위치 까지의 합계
 		int sum = 0;
-		for(int i = a - 1; i < b - 1; i++) {
+		for(int i = a-1; i < b-1; i++) {
 			sum += array[i];
 		}
 		
