ea8fe35c6ee6b39cac44db762ea5f3569590fe76
khw64 <khw64@LAPTOP-PPCB0B8V>
2022-11-06 20:36:53 +0900

    짝수의 합

---
+++ Programmers/src/week1/EvenNumberSum.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class EvenNumberSum {
+
+	public int solution(int n) {
+		int answer = 0;
+		for(int i = 2; i <= n; i++) {
+			if(i % 2 == 0) answer += i;
+		}
+		return answer;
+	}
+	
+}
