28fd0bf74c5c5d98e0770971c6f3ce676c21c8c0
khw64 <khw64@LAPTOP-PPCB0B8V>
2022-11-06 20:38:56 +0900

    머쓱이보다 키 큰 사람

---
+++ Programmers/src/week1/TallerThenM.java
@@ -0,0 +1,12 @@
+package week1;
+
+public class TallerThenM {
+	
+	public int solution(int[] array, int height) {
+        int answer = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] > height) answer++;
+        }
+        return answer;
+    }
+}
