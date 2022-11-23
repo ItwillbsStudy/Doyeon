b4feba132f85b7095fc0be03ad1c8f2975f13ec8
khw64 <khw64@LAPTOP-PPCB0B8V>
2022-11-23 21:51:20 +0900

    구슬 선택

--- Programmers/src/week5/Marble.java
+++ Programmers/src/week5/Marble.java
@@ -1,5 +1,7 @@
 package week5;
 
+import java.math.BigInteger;
+
 public class Marble {
 
 	public static void main(String[] args) {
@@ -14,35 +16,39 @@ public class Marble {
 		int balls = 5;
 		int share = 3;
 		//return 값
-		long ballsp = 1L;
-		long sharep = 1L;
-		long bmsp = 1L;
+		BigInteger ballsp = new BigInteger("1");
+		BigInteger sharep = new BigInteger("1");
+		BigInteger bmsp = new BigInteger("1");
 		
-		int answer = 0;
+		BigInteger answer = new BigInteger("0");
 		
 		if((balls/2) > share) {
 			//조건: balls > (balls-share)
 			//그거를 share로 나눔
 			for(int i = balls; i > (balls-share); i--){
-				ballsp *= i;
+				BigInteger index = new BigInteger("i");
+				ballsp = ballsp.multiply(index);
 			}
 			for(int i = share; i > 0; i--) {
-				sharep *= i;
+				BigInteger index = new BigInteger("i");
+				sharep = sharep.multiply(index);
 			}
-			answer = (int)(ballsp / sharep);
+			answer = ballsp.divide(sharep);
 		}else {
 			//조건: balls > share
 			//그거를 balls-share로 나눔
 			for(int i = balls; i > share; i--){
-				ballsp *= i;
+				BigInteger index = new BigInteger("i");
+				ballsp = ballsp.multiply(index);
 			}
 			for(int i = (balls-share); i > 0; i--) {
-				bmsp *= i;
+				BigInteger index = new BigInteger("i");
+				bmsp = bmsp.multiply(index);
 			}
-			answer = (int)(ballsp / bmsp);
+			answer = ballsp.divide(bmsp);
 		}
 		
-		System.out.println(answer);
+		System.out.println(answer.intValue());
 	}
 
 }
